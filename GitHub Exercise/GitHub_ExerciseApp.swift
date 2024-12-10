accbdbb76d3b49f14cbcf7627edb258fa40267a2
Louis Kudelka <aelnk1954@gmail.com>
2024-12-10 14:35:57 -0500

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Mbam3 on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
